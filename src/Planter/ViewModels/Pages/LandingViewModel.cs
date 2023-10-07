43df8bb6a87eec32e973cfb8e91d3e1ba9fe12a2
June <junimiso04@naver.com>
2023-10-08 03:56:20 +0900

    feat: update landing page

--- src/Planter/ViewModels/Pages/LandingViewModel.cs
+++ src/Planter/ViewModels/Pages/LandingViewModel.cs
@@ -1,17 +1,53 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using Planter.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Planter.ViewModels.Pages
 {
     public class LandingViewModel : ViewModelBase
     {
+        public ICommand NavigateToGitHubCommand { get; private set; }
+
+        public ICommand NavigateToGitHubReleasesCommand { get; private set; }
+
+        public ICommand NavigateToUserManualCommand { get; private set; }
+
+        public ICommand NavigateToNextInteractionCommand { get; private set; }
+
         public LandingViewModel()
         {
             Title = "Landing Page";
             Description = "A Planter landing page";
+
+            NavigateToGitHubCommand = new RelayCommand(OnNavigateToGitHub);
+            NavigateToGitHubReleasesCommand = new RelayCommand(OnNavigateToGitHubReleases);
+            NavigateToUserManualCommand = new RelayCommand(OnNavigateToUserManual);
+            NavigateToNextInteractionCommand = new RelayCommand(OnNavigateToNextInteraction);
+        }
+
+        private void OnNavigateToGitHub()
+        {
+            UriHelper.Open("https://github.com/root-square/planter");
+        }
+
+        private void OnNavigateToGitHubReleases()
+        {
+            UriHelper.Open("https://github.com/root-square/planter/releases");
+        }
+
+        private void OnNavigateToUserManual()
+        {
+            UriHelper.Open("https://github.com/root-square/planter");
+        }
+
+        private void OnNavigateToNextInteraction()
+        {
+            
         }
     }
 }
