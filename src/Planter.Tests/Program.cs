e26497da63da4da83673fed98e483166efd6d056
June <junimiso04@naver.com>
2023-10-08 00:29:09 +0900

    feat: add initial projects

---
+++ src/Planter.Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Planter.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
